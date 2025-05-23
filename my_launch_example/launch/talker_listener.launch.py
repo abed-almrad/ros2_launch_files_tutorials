16e14cbca5b3aba5dc206e65565d2166bb804b2c
Abed <robotogeddon@gmail.com>
2025-05-22 20:56:25 -0400

    Added arguments and conditions to the launch file

--- my_launch_example/launch/talker_listener.launch.py
+++ my_launch_example/launch/talker_listener.launch.py
@@ -1,18 +1,36 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
 
 def generate_launch_description():
+    topic_arg = LaunchConfiguration('chatter_topic')
+
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'chatter_topic',
+            default_value='chatter',
+            description='Topic name for talker and listener nodes'
+        ),
+        DeclareLaunchArgument(
+            'start_listener',
+            default_value='true',
+            description='Whether to launch the listener node or not'
+        ),
         Node(
             package='demo_nodes_cpp',
             executable='talker',
-            name='my_talker'
+            name='my_talker',
+            remappings=[('/chatter', topic_arg)]
         ),
 
         Node(
             package='demo_nodes_cpp',
             executable='listener',
-            name='my_listener'
+            name='my_listener',
+            remappings=[('/chatter', topic_arg)],
+            condition=IfCondition(LaunchConfiguration('start_listener'))
         )
 
     ])
